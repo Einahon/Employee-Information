62f8ac405c7b2fc55e3ababe56c8993aca59ef72
elie <inahon.elie17@yahoo.com>
2024-04-05 15:53:41 -0400

    first commit

---
+++ src/main/java/com/eliehome/EmployeeInformation/configuration/EmployeeServiceConfig.java
@@ -0,0 +1,8 @@
+package com.eliehome.EmployeeInformation.configuration;
+
+import com.eliehome.EmployeeInformation.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceConfig implements EmployeeService {
+}
