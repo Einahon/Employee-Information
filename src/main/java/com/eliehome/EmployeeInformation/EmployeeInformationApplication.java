b68cfdec51cc441e010e228e427c5da6fb8dc532
elie <inahon.elie17@yahoo.com>
2024-04-05 13:53:47 -0400

    first commit

---
+++ src/main/java/com/eliehome/EmployeeInformation/EmployeeInformationApplication.java
@@ -0,0 +1,14 @@
+package com.eliehome.EmployeeInformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeInformationApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EmployeeInformationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eliehome/EmployeeInformation/EmployeeInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliehome.EmployeeInformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
