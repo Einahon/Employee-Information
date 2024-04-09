c32341ea587bc0819f5a6911e215ccd7ec5a4aa3
elie <inahon.elie17@yahoo.com>
2024-04-08 22:50:12 -0400

    first commit

--- src/main/java/com/eliehome/EmployeeInformation/service/EmployeeService.java
+++ src/main/java/com/eliehome/EmployeeInformation/service/EmployeeService.java
@@ -3,16 +3,17 @@ package com.eliehome.EmployeeInformation.service;
 import com.eliehome.EmployeeInformation.model.Employee;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface EmployeeService {
    public Employee saveEmployee(Employee employee);
 
-  public List<Employee> listOfEmployees();
+  public List<Employee> fetchEmployeeList();
 
-   public Employee getEmployeeById(Long employeeId);
+   public Employee fetchEmployeeById(Long employeeId);
 
    public void deleteEmployeeById(Long employeeId);
+
+   public Employee updateEmployee(Long employeeId, Employee employee);
 
 
 //   public Employee updateEmployeeById(Long employeeId, Employee employee);
