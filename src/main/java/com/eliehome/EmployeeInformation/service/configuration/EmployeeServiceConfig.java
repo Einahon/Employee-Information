073686ddea47a24342cd2138aad69850b34dcd77
elie <inahon.elie17@yahoo.com>
2024-04-13 00:17:30 -0400

    first commit

--- src/main/java/com/eliehome/EmployeeInformation/service/configuration/EmployeeServiceConfig.java
+++ src/main/java/com/eliehome/EmployeeInformation/service/configuration/EmployeeServiceConfig.java
@@ -1,5 +1,6 @@
 package com.eliehome.EmployeeInformation.service.configuration;
 
+import com.eliehome.EmployeeInformation.error.EmployeeNotFoundException;
 import com.eliehome.EmployeeInformation.model.Employee;
 import com.eliehome.EmployeeInformation.repository.EmployeeRepository;
 import com.eliehome.EmployeeInformation.service.EmployeeService;
@@ -26,9 +27,14 @@ public class EmployeeServiceConfig implements EmployeeService {
     }
 
     @Override
-    public Employee fetchEmployeeById(Long employeeId) {
+    public Employee fetchEmployeeById(Long employeeId) throws EmployeeNotFoundException {
 
-        return employeeRepository.findById(employeeId).get();
+        Optional<Employee> employee = employeeRepository.findById(employeeId);
+
+        if(!employee.isPresent()){
+            throw new EmployeeNotFoundException("Employee Not Available");
+        }
+        return employee.get();
     }
 
     @Override
