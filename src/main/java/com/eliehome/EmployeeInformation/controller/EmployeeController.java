1b04278a0e642167bde8411388e685fe58ded33e
elie <inahon.elie17@yahoo.com>
2024-04-12 23:34:44 -0400

    first commit

--- src/main/java/com/eliehome/EmployeeInformation/controller/EmployeeController.java
+++ src/main/java/com/eliehome/EmployeeInformation/controller/EmployeeController.java
@@ -3,6 +3,8 @@ package com.eliehome.EmployeeInformation.controller;
 import com.eliehome.EmployeeInformation.model.Employee;
 import com.eliehome.EmployeeInformation.service.EmployeeService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,23 +16,28 @@ import java.util.List;
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
+
+    private final Logger LOGGER = LoggerFactory.getLogger(EmployeeController.class);
     @PostMapping("/employees")
     public Employee saveEmployee(@Valid @RequestBody Employee employee){
+        LOGGER.info("Inside saveEmployee of EmployeeController");
         return employeeService.saveEmployee(employee);
 
     }
     @GetMapping("/employees")
     public List<Employee> fetchEmployeeList() {
-
+     LOGGER.info("Inside fetchEmployeeList of EmployeeController");
         return employeeService.fetchEmployeeList();
     }
     @GetMapping("/employees/{id}")
     public Employee fetchEmployeeById(@PathVariable("id") Long employee_Id){
+        LOGGER.info("Inside fetchEmployeeById of EmployeeController ");
             return employeeService.fetchEmployeeById(employee_Id);
     }
 
     @DeleteMapping("/employees/{id}")
     public String deleteEmployeeById(@PathVariable("id") Long employee_Id){
+        LOGGER.info("Inside deleteEmployeeById of EmployeeController ");
          employeeService.deleteEmployeeById(employee_Id);
          return "Employee Deleted Successfully!!";
 
