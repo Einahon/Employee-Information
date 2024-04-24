d873d0cee94c092666c1f4d012a984feab771e43
elie <inahon.elie17@yahoo.com>
2024-04-24 18:35:42 -0400

    first commit

---
+++ src/main/java/com/eliehome/EmployeeInformation/model/ErrorMessage.java
@@ -0,0 +1,13 @@
+package com.eliehome.EmployeeInformation.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorMessage {
+    private HttpStatus status;
+    private String message;
+}
