119e95b24bd4e3e97c707e690301ffbc2604871c
elie <inahon.elie17@yahoo.com>
2024-05-08 01:58:33 -0400

    first commit

--- src/main/java/com/eliehome/EmployeeInformation/model/Employee.java
+++ src/main/java/com/eliehome/EmployeeInformation/model/Employee.java
@@ -4,9 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,15 +16,13 @@ import org.hibernate.validator.constraints.Length;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long employeeId;
 
-    @Valid
+
     @NotBlank(message = "Please add name")
-    @NotNull(message = "Please add name")
     private String employeeName;
 
     @Length(max = 12, min = 8, message = "Must be of 8 - 12 digit")
@@ -34,7 +30,5 @@ public class Employee {
 
     @Length(max = 25, min = 1, message = "Must be of 1 - 25 characters")
     private String employeeDepartment;
-
-
 
 }
