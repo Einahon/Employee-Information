2de038c3442ebac8d0e09d30c3d1c37c22d02df6
elie <inahon.elie17@yahoo.com>
2024-05-08 02:00:17 -0400

    first commit

--- src/main/java/com/eliehome/EmployeeInformation/controller/EmployeeController.java
+++ src/main/java/com/eliehome/EmployeeInformation/controller/EmployeeController.java
@@ -2,28 +2,25 @@ package com.eliehome.EmployeeInformation.controller;
 
 import com.eliehome.EmployeeInformation.error.EmployeeNotFoundException;
 import com.eliehome.EmployeeInformation.model.Employee;
-import com.eliehome.EmployeeInformation.service.EmployeeService;
-import jakarta.validation.Valid;
+import com.eliehome.EmployeeInformation.service.IEmployeeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 
 public class EmployeeController {
     @Autowired
-    private EmployeeService employeeService;
+    private IEmployeeService employeeService;
 
     private final Logger LOGGER = LoggerFactory.getLogger(EmployeeController.class);
     @PostMapping("/employees")
     @ResponseStatus(HttpStatus.CREATED)
-    public Employee saveEmployee(@Valid @RequestBody Employee employee) {
+    public Employee saveEmployee(@Validated @RequestBody Employee employee) {
         LOGGER.info("Inside saveEmployee of EmployeeController");
         return employeeService.saveEmployee(employee);
 
@@ -39,14 +36,13 @@ public class EmployeeController {
             return employeeService.fetchEmployeeById(employeeId);
     }
     @DeleteMapping("/employees/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
     public String deleteEmployeeById(@PathVariable("id") Long employeeId) throws EmployeeNotFoundException {
         LOGGER.info("Inside deleteEmployeeById of EmployeeController ");
          employeeService.deleteEmployeeById(employeeId);
          return "Employee Deleted Successfully!!";
     }
     @PutMapping("/employees/{id}")
-    public Employee updateEmployee(@PathVariable("id") Long employeeId,@Valid @RequestBody Employee employee) throws EmployeeNotFoundException {
+    public Employee updateEmployee(@PathVariable("id") Long employeeId,@Validated @RequestBody Employee employee) throws EmployeeNotFoundException {
         return employeeService.updateEmployee(employeeId, employee);
     }
     @GetMapping("/employees/name/{name}")
